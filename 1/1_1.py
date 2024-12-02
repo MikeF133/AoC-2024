eb755ff1e2832a62117bfe0aa348ebad4e1e30ac
Mike F <atronarh@gmail.com>
2024-12-02 17:55:55 +0300

    1_2 added

--- 1/1_1.py
+++ 1/1_1.py
@@ -1,2 +1,15 @@
-def Solution():
-    pass
+left, right = [], []
+with open("data.txt", "r") as fopen:
+    for line in fopen:
+        nums = line.split()
+        left.append(int(nums[0]))
+        right.append(int(nums[1]))
+
+left = sorted(left, reverse=True)
+right = sorted(right, reverse=True)
+
+total = 0
+for i in range(len(left)):
+    total += abs(left[i] - right[i])
+
+print(total)
--- 1/1_2.py
+++ 1/1_2.py
@@ -1,2 +1,16 @@
-def Solution():
-    pass
+left, right = [], []
+with open("data.txt", "r") as fopen:
+    for line in fopen:
+        nums = line.split()
+        left.append(int(nums[0]))
+        right.append(int(nums[1]))
+
+right_count = dict()
+for num in right:
+    right_count[num] = right_count.get(num, 0) + 1
+
+total = 0
+for num in left:
+    total += num * right_count.get(num, 0)
+
+print(total)
